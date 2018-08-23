a9322cf9170fc196957699930b0df386af836968
Chris Calloway <cbc@chriscalloway.org>
2018-08-23 21:46:59 +0000

    Add safeguard to /etc/hosts.d

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -20,16 +20,20 @@ fi
 echo "jenkins ALL=(root) NOPASSWD: /usr/bin/docker" > /etc/sudoers.d/jenkins
 chmod 0440 /etc/sudoers.d/jenkins
 
-for FILENAME in /etc/hosts.d/*.conf; do
-    FIRST=$(head -n 1 "$FILENAME")
-    if ! grep -xq "$FIRST" /etc/hosts; then
-       printf "\n" >> /etc/hosts
-       cat ${FILENAME} >> /etc/hosts
-       echo Concatenated ${FILENAME} to /etc/hosts
-    else
-        echo ${FILENAME} already concatenated to /etc/hosts 
-    fi
-done
+HOSTS=/etc/hosts
+HOSTSD=/etc/hosts.d
+if [ -d "$HOSTSD" ]; then
+    for FILENAME in ${HOSTSD}/*.conf; do
+        FIRST=$(head -n 1 "$FILENAME")
+        if ! grep -xq "$FIRST" ${HOSTS}; then
+            printf "\n" >> ${HOSTS}
+            cat ${FILENAME} >> ${HOSTS}
+            echo Concatenated ${FILENAME} to ${HOSTS}
+        else
+            echo ${FILENAME} already concatenated to ${HOSTS}
+        fi
+    done
+fi
 
 # run Jenkins as user jenkins
 su jenkins -c 'cd $HOME; /usr/local/bin/jenkins.sh'
